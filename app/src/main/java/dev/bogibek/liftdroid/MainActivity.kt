6ff4459b5b35f7e17faff8454039ab0a60d5168f
BogibekDev <bogibekdev@gmail.com>
2022-07-18 19:02:36 +0000

    initial commit

---
+++ app/src/main/java/dev/bogibek/liftdroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.bogibek.liftdroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
