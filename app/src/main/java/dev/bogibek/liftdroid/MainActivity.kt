c45315178abdbfb97b936caba02e1ffd6c2bb3a1
BogibekDev <bogibekdev@gmail.com>
2022-07-18 19:36:36 +0000

    binding added

--- app/src/main/java/dev/bogibek/liftdroid/MainActivity.kt
+++ app/src/main/java/dev/bogibek/liftdroid/MainActivity.kt
@@ -1,11 +1,18 @@
 package dev.bogibek.liftdroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.bogibek.liftdroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupUi()
+    }
+
+    private fun setupUi() {
+
     }
 }
